eb5ede26686a54d31aa131b52dae406f4237a79c
PankhuriBansal <66808553+PankhuriBansal@users.noreply.github.com>
2023-04-26 01:18:56 +0530

    Add files via upload

---
+++ build/precache-manifest.35a3484ef06b21c34c148ed75495871f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "97d7aabd09a6800318f2",
+    "url": "/static/css/main.f474334c.chunk.css"
+  },
+  {
+    "revision": "97d7aabd09a6800318f2",
+    "url": "/static/js/main.97d7aabd.chunk.js"
+  },
+  {
+    "revision": "08ba074acee96e12e878",
+    "url": "/static/css/1.e2645b76.chunk.css"
+  },
+  {
+    "revision": "08ba074acee96e12e878",
+    "url": "/static/js/1.08ba074a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "473f0b49a5f74b8df38d4046b745ff89",
+    "url": "/index.html"
+  }
+];
